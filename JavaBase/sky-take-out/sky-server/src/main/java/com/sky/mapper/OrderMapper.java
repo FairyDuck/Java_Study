6ece0be24106c08f5b83170034ae31dbd686e2d0
fairyduck <14820104+fairyduck@user.noreply.gitee.com>
2026-08-31 22:04:12 +0800

    苍穹外卖-部分接口

--- JavaBase/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ JavaBase/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,7 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +16,33 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 分页查询订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     * @return
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
+    /**
+     * 修改订单
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 根据状态查询订单数量
+     * @param status
+     * @return
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
